5bff2d9ef421bd64bb11b7dc22184cba34f371d5
AlexLitvino <litvinov.aleks@gmail.com>
2022-02-12 22:38:04 +0200

    added task structure without list

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # fun_tasks_al
-Interesting tasks to share
+Here I will upload interesting tasks to solve.  
+
+- [Structure without list](src/structure_without_list.py)  
+Task provided by Alexander Pushkarev in [video](https://www.youtube.com/watch?v=-Qx3-2oZpVY) on his YouTube channel  
+  Develop class that contains tree public methods:
+  - void add(item) - adding any object to this class;  
+  - object get(index) - getting previously added item. The first item will be returned by get(1), the second - get(2), and so on;  
+  - void delete(index) - deleting previously added iem by its sequence number.  
+
+Special condition: there're no lists, arrays, collections in programming language you will use (or simply don't use them).
---
+++ src/structure_without_list.py
@@ -0,0 +1,118 @@
+class StructureWithoutList:
+
+    def __init__(self):
+        self._reset()
+
+    def add(self, value):
+        """ Adding new element """
+        if self.length == 0:
+            self.current_item = Item(value, prev_item=None, next_item=None)
+        else:
+            new_item = Item(value, prev_item=self.current_item, next_item=None)
+            self.current_item.next_item = new_item
+            self.current_item = new_item
+        self.length += 1
+        self.current_index += 1
+
+    def get(self, index):
+        """ Getting value by index """
+        if 1 <= index <= self.length:
+            delta = self.current_index - index
+            if delta >= 0:  # current_index > index, we ahead of destination
+                for i in range(delta):
+                    self._go_previous()
+            else:
+                for i in range(-delta):
+                    self._go_next()
+            return self.current_item.value
+        else:
+            raise IndexError
+
+    def delete(self, index):
+        """ Deleting element by index """
+        if 1 <= index <= self.length:
+            delta = self.current_index - index
+            if delta >= 0:   # current_index > index, we ahead of destination
+                for i in range(delta):
+                    self._go_previous()
+            else:
+                for i in range(-delta):
+                    self._go_next()
+
+            if self.length == 1:
+                self._reset()
+            else:
+                if self.current_index == 1:
+                    next_item = self.current_item.next_item
+                    del self.current_item
+                    self.current_item = next_item
+                    self.current_item.prev_item = None
+                    self.current_index = 1
+                elif self.current_index == self.length:
+                    prev_item = self.current_item.prev_item
+                    del self.current_item
+                    self.current_item = prev_item
+                    self.current_item.next_item = None
+                    self.current_index = self.length - 1
+                else:
+                    prev_item = self.current_item.prev_item
+                    next_item = self.current_item.next_item
+                    prev_item.next_item = next_item
+                    next_item.prev_item = prev_item
+                    del self.current_item
+                    self.current_item = prev_item
+                    self.current_index -= 1
+                self.length -= 1
+        else:
+            raise IndexError
+
+    def _reset(self):
+        """ Sets collection initial values at the initialization and when removing the only element """
+        self.length = 0
+        self.current_item = None
+        self.current_index = 0
+
+    def _go_previous(self):
+        """ Go to previous item in collection """
+        prev_item = self.current_item.prev_item
+        self.current_item = prev_item
+        self.current_index -= 1
+
+    def _go_next(self):
+        """ Go to next item in collection """
+        next_item = self.current_item.next_item
+        self.current_item = next_item
+        self.current_index += 1
+
+    def get_content_as_list(self):
+        """ Returns all elements added to class as list of item values"""
+        return [self.get(i+1) for i in range(self.length)]
+
+
+class Item:
+    """ Wrapper for values added to collection to keep added value and links to previous and next items """
+    def __init__(self, value, prev_item, next_item):
+        self.value = value
+        self.prev_item = prev_item
+        self.next_item = next_item
+
+
+if __name__ == "__main__":
+    s = StructureWithoutList()
+    print(s.get_content_as_list())
+    s.add(1)
+    print(s.get_content_as_list())
+    s.add(2)
+    print(s.get_content_as_list())
+    s.add(3)
+    print(s.get_content_as_list())
+    print(f"get(1) = {s.get(1)}")
+    print(f"get(3) = {s.get(3)}")
+    print(f"get(2) = {s.get(2)}")
+    print(s.get_content_as_list())
+    s.delete(3)
+    print(s.get_content_as_list())
+    s.delete(2)
+    print(s.get_content_as_list())
+    s.delete(1)
+    print(s.get_content_as_list())
